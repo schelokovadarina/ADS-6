8862e9b19252e30a504ad94c101e612c73affab4
schelokovadarina <99324863+schelokovadarina@users.noreply.github.com>
2022-04-10 21:21:30 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,30 +2,28 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
+
 template<typename T, int size>
 class TPQueue {
 private:
 T arr[size] = { 0 };
-int  first=0, last=0;
+int first, last;
 
-public: TPQueue() :first{ 0 }, last{ 0 }  {};
+public:
+TPQueue() : first(0), last(0) {}
 void push(T temp) {
-int i = last;
-while ((--i >= first) && (value.prior > arr[i % size].prior)) {
+int i = last++;
+while ((--i >= first) && (arr[i % size].prior < temp.prior)) {
 arr[(i + 1) % size] = arr[i % size];
 }
-arr[(i + 1) % size] = value;
-last++;
+arr[(i + 1) % size] = temp;
 }
-};
 T pop() {
 return arr[(first++) % size];
 }
 };
-
 struct SYM {
-char ch;
-int prior;
+	char ch;
+	int prior;
 };
-
 #endif  // INCLUDE_TPQUEUE_H_
